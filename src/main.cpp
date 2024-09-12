1eeddb673333a6b945727771ea20709f89e3359d
Marcus Cauê <marcuscaue.dev@gmail.com>
2024-09-12 11:16:30 -0300

    Novos Sprites

--- src/main.cpp
+++ src/main.cpp
@@ -10,63 +10,7 @@
 using namespace std;
 
 // Tela de "seleção de monstro"
-/* for (Sprite &sp : sps) {
-	system("clear");
-
-	luff.update();
-	corvo.update();
-	leao.update();
-	serp.update();
-
-	// draw(linha, coluna)
-	luff.draw(tela, 0, 0);
-	corvo.draw(tela, 0, 110);
-	leao.draw(tela, 2, 202);
-	serp.draw(tela, 2, 293);
-
-	cout << tela << endl;
-	getline(cin, str);
-} */
-
-// Seta de seleção
-/* SpriteBuffer tela = SpriteBuffer(100, 10);
-
-string str;
-
-ObjetoDeJogo hab = ObjetoDeJogo("Habilidades", TextSprite("HABILIDADES"), 0, 10);
-ObjetoDeJogo itens = ObjetoDeJogo("Itens", TextSprite("ITEMS"), 1, 10);
-ObjetoDeJogo habSp = ObjetoDeJogo("Especial", TextSprite("ESPECIAL"), 2, 10);
-ObjetoDeJogo sleep = ObjetoDeJogo("Descansar", TextSprite("DESCANSAR"), 3, 10);
-
-ObjetoDeJogo seta = ObjetoDeJogo("Seta de seleção", TextSprite("->"), 0, 5);
-
-while (true) {
-	getline(cin, str);
-
-	if (str == "s") {
-		seta.moveDown();
-	} else if (str == "w") {
-		seta.moveUp();
-	}
-
-	seta.update();
-	tela.clear();
-
-	itens.draw(tela, itens.getPosL(), itens.getPosC());
-	hab.draw(tela, hab.getPosL(), hab.getPosC());
-	habSp.draw(tela, habSp.getPosL(), habSp.getPosC());
-	sleep.draw(tela, sleep.getPosL(), sleep.getPosC());
-
-	seta.draw(tela, seta.getPosL(), seta.getPosC());
-
-	system("clear");
-	cout << tela << endl;
-	cout << "Posição da seta: " << seta.getPosL() << "x" << seta.getPosC() << endl;
-
-} */
-
-int main() {
-
+void selectMonster() {
 	SpriteBuffer tela = SpriteBuffer(200, 30);
 
 	Sprite luff = Sprite("./sprites/monsters/texugo/texugo.sp");
@@ -75,22 +19,65 @@ int main() {
 	Sprite serpente = Sprite("./sprites/monsters/serpente/serpente-in-battle.sp");
 
 	string str;
-	
+
 	while (true) {
 		getline(cin, str);
 
 		luff.update();
 		leao.update();
-	
+
 		// draw(linha, coluna)
 		luff.draw(tela, 0, 0);
 		leao.draw(tela, 0, 50);
 		serpente.draw(tela, 0, 100);
 		corvo.draw(tela, 0, 150);
-	
+
 		system("clear");
 		cout << tela << endl;
 	}
+}
+
+// Seta de seleção
+void seta() {
+	SpriteBuffer tela = SpriteBuffer(100, 10);
+	
+	string str;
+	
+	ObjetoDeJogo hab = ObjetoDeJogo("Habilidades", TextSprite("HABILIDADES"), 0, 10);
+	ObjetoDeJogo itens = ObjetoDeJogo("Itens", TextSprite("ITEMS"), 1, 10);
+	ObjetoDeJogo habSp = ObjetoDeJogo("Especial", TextSprite("ESPECIAL"), 2, 10);
+	ObjetoDeJogo sleep = ObjetoDeJogo("Descansar", TextSprite("DESCANSAR"), 3, 10);
+	
+	ObjetoDeJogo seta = ObjetoDeJogo("Seta de seleção", TextSprite("->"), 0, 5);
+	
+	while (true) {
+		getline(cin, str);
+	
+		if (str == "s") {
+			seta.moveDown();
+		} else if (str == "w") {
+			seta.moveUp();
+		}
+	
+		seta.update();
+		tela.clear();
+	
+		itens.draw(tela, itens.getPosL(), itens.getPosC());
+		hab.draw(tela, hab.getPosL(), hab.getPosC());
+		habSp.draw(tela, habSp.getPosL(), habSp.getPosC());
+		sleep.draw(tela, sleep.getPosL(), sleep.getPosC());
+	
+		seta.draw(tela, seta.getPosL(), seta.getPosC());
+	
+		system("clear");
+		cout << tela << endl;
+		cout << "Posição da seta: " << seta.getPosL() << "x" << seta.getPosC() << endl;
+	
+	}
+}
+
+int main() {
+	
 	
 	return 0;
 }
